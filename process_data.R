33f21f5685f29be59c943328086cbb389b692e75
Gavin Rozzi <gr@gavinrozzi.com>
2020-06-06 01:52:49 -0400

    Initial working version
    First version, allows comparison between states

---
+++ process_data.R
@@ -0,0 +1,15 @@
+library(plyr)
+library(readxl)
+
+# Convert the 1033 Program excel sheet to a CSV file for analysis.
+
+# List all sheets
+sheets <- excel_sheets('data/DISP_AllStatesAndTerritories_03312020.xlsx')
+
+# Read in all sheets to a list
+list <- lapply(sheets, function(x) read_excel('data/DISP_AllStatesAndTerritories_03312020.xlsx', sheet = x))
+
+# Convert to a single dataframe
+df <- rbind.fill(list)
+
+write.csv(df, 'data/data.csv')
