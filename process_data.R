fea03bc090014cfb5d1b5c3564f128e98286397c
Gavin Rozzi <gr@gavinrozzi.com>
2020-08-03 22:44:06 -0400

    Quarterly data update

--- process_data.R
+++ process_data.R
@@ -4,7 +4,7 @@ library(readxl)
 # Convert the 1033 Program excel sheet to a CSV file for analysis.
 
 # List all sheets
-sheets <- excel_sheets('data/DISP_AllStatesAndTerritories_03312020.xlsx')
+sheets <- excel_sheets('data/DISP_AllStatesAndTerritories_06302020.xlsx')
 
 # Read in all sheets to a list
 list <- lapply(sheets, function(x) read_excel('data/DISP_AllStatesAndTerritories_03312020.xlsx', sheet = x))
